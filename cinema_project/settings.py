ac9cd5258d54b2a8e27aed173398dc4eb6cee8fd
alafyte <xleb.xlebnyj@mail.ru>
2023-08-10 20:32:59 +0300

    add cinema_app in application list

--- cinema_project/settings.py
+++ cinema_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cinema_app.apps.CinemaAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
